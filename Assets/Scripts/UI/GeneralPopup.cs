376ca5edcea11d4b8dc10e54c541b7e3d68eb224
Harjot Singh <121034602+deadlockhook@users.noreply.github.com>
2025-01-09 16:02:15 -0400

    Added popup logic script

--- Assets/Scripts/UI/GeneralPopup.cs
+++ Assets/Scripts/UI/GeneralPopup.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,19 +10,42 @@ public class GeneralPopup : MonoBehaviour
     // Start is called before the first frame update
     private IngameUIManager ingameUIManager;
     private List<ImageWithState> popupImages;
-    public Sprite TestSprite;
+    public int popupElementSize = 50;
+    public int widthPerElement = 2;
+    public int maxElementsPerRow = 3;
+    public int maxColumns = 3;
+
+    public Sprite TestSpriteActive;
+    public Sprite TestSpriteInactive;
+
+    private int rowSize = 0;
+    private int columnSize = 0;
     void Start()
     {
+        rowSize = ((popupElementSize + widthPerElement) + widthPerElement) * maxElementsPerRow;
+        columnSize = ((popupElementSize + widthPerElement) + widthPerElement) * maxColumns;
         ingameUIManager = FindAnyObjectByType<IngameUIManager>();
         popupImages = new List<ImageWithState>();
-        PushElement(TestSprite,TestSprite);
-        PushElement(TestSprite, TestSprite);
-        PushElement(TestSprite, TestSprite);
+        PushElement(TestSpriteActive, TestSpriteInactive);
+        PushElement(TestSpriteActive, TestSpriteInactive);
+        PushElement(TestSpriteActive, TestSpriteInactive);
+        PushElement(TestSpriteActive, TestSpriteInactive);
+        PushElement(TestSpriteActive, TestSpriteInactive);
+        PushElement(TestSpriteActive, TestSpriteInactive);
+        PushElement(TestSpriteActive, TestSpriteInactive);
+
+
+        RectTransform backgroundImageRectTransform = transform.Find("BG").GetComponent<RectTransform>();
+        RectTransform parentRectTransform = this.gameObject.GetComponent<RectTransform>();
+   //     backgroundImageRectTransform.anchoredPosition = parentRectTransform.anchoredPosition;
+        backgroundImageRectTransform.position = parentRectTransform.position;
+
+        backgroundImageRectTransform.sizeDelta = new Vector2(rowSize/ 2.0f, columnSize / 2.0f);
     }
 
     public void PushElement(Sprite activeSprite,Sprite inactiveSprite)
     {
-        ImageWithState imageObj = ingameUIManager.CreateImageWithState(this.gameObject.transform, activeSprite, inactiveSprite);
+        ImageWithState imageObj = ingameUIManager.CreateImageWithStateEx(this.gameObject.transform, activeSprite, inactiveSprite, false, new Vector2(popupElementSize, popupElementSize));
 
         if (imageObj)
         {
@@ -38,26 +62,27 @@ public class GeneralPopup : MonoBehaviour
     }
     void SetupPositions()
     {
-        ImageWithState prevItem = null;
+        int currentElementInRow = 0;
+        int currentColumn = 0;
+
+        int currentElementIndex = 0;
+
+        Vector2 thisObjectRectTransformPosition = this.gameObject.GetComponent<RectTransform>().position;
+
         foreach (var currentItem in popupImages)
         {
+            if (currentElementIndex == maxElementsPerRow)
+            {
+                currentColumn++;
+                currentElementIndex = 0;
+            }
+
             RectTransform currentItemRectTransform = currentItem.GetComponent<RectTransform>();
+            Vector2 currentItemRectTransformPosition = currentItemRectTransform.position;
+            Vector2 currentItemRectTransformSize = currentItemRectTransform.sizeDelta;
+            currentItemRectTransform.position = new Vector2(thisObjectRectTransformPosition.x + ((widthPerElement + currentItemRectTransformSize.x) * currentElementIndex), thisObjectRectTransformPosition.y + ((widthPerElement + (currentItemRectTransformSize.y * -1)) * currentColumn));
 
-            if (prevItem == null)
-            {
-                currentItemRectTransform.position = this.gameObject.GetComponent<RectTransform>().position;
-            }
-            else
-            {
-                RectTransform prevItemRectTransform = prevItem.GetComponent<RectTransform>();
-
-                Vector2 prevItemRectTransformPosition = prevItemRectTransform.position;
-                Vector2 prevItemRectTransformSize = prevItemRectTransform.sizeDelta;
-
-                currentItemRectTransform.GetComponent<RectTransform>().position = new Vector2(prevItemRectTransformPosition.x + prevItemRectTransformSize.x, prevItemRectTransformPosition.y + prevItemRectTransformSize.x);
-            }
-
-            prevItem = currentItem;
+            ++currentElementIndex;
         }
     }
 
