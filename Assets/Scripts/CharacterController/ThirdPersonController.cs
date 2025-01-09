64132ff7d90fd74a38ea80f532ff93fe2e514193
Arman <144450534+JoeMac12@users.noreply.github.com>
2025-01-09 14:00:07 -0400

    Player State Machine

--- Assets/Scripts/CharacterController/ThirdPersonController.cs
+++ Assets/Scripts/CharacterController/ThirdPersonController.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 using UnityEngine.AI;
 
 /*
- * Credits - Joseph MacDonald
- * NSCC - Game Programming Student
- * This script handles logic for player movement using unity navmesh and clicking on enviroment
+	* Credits - Joseph MacDonald
+	* NSCC - Game Programming Student
+	* This script handles logic for player movement using unity navmesh and clicking on enviroment
 */
 
 public class ThirdPersonController : MonoBehaviour
 {
 	private NavMeshAgent agent;
 	private Camera mainCamera;
+
+	private enum MovementState
+	{
+		Stopped,
+		Moving
+	}
+
+	// Used for checking if player is actually moving
+	private MovementState currentState = MovementState.Stopped;
+	[SerializeField] private float velocityThreshold = 0.1f;
 
 	void Start()
 	{
@@ -32,6 +42,28 @@ public class ThirdPersonController : MonoBehaviour
 			{
 				agent.SetDestination(hit.point);
 			}
+		}
+
+		// Update state machine based on vel (This is also how we are gonna trigger player animations)
+		switch (currentState)
+		{
+			case MovementState.Stopped:
+				// Check if moving
+				if (agent.velocity.magnitude > velocityThreshold)
+				{
+					currentState = MovementState.Moving;
+					Debug.Log("Moving");
+				}
+				break;
+
+			case MovementState.Moving:
+				// Check if stopped
+				if (agent.velocity.magnitude <= velocityThreshold)
+				{
+					currentState = MovementState.Stopped;
+					Debug.Log("Stopped");
+				}
+				break;
 		}
 	}
 }
