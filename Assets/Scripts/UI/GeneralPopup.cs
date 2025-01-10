f24c31f510f1c58bc353ba2bc36783faee830574
Harjot Singh <121034602+deadlockhook@users.noreply.github.com>
2025-01-10 10:48:58 -0400

    Added background for popups, OnExit Logic and ActionButton Listeners

--- Assets/Scripts/UI/GeneralPopup.cs
+++ Assets/Scripts/UI/GeneralPopup.cs
@@ -2,7 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
 
 public class GeneralPopup : MonoBehaviour
@@ -11,7 +13,7 @@ public class GeneralPopup : MonoBehaviour
     private IngameUIManager ingameUIManager;
     private List<ImageWithState> popupImages;
     public int popupElementSize = 50;
-    public int widthPerElement = 2;
+    private int widthPerElement = 5;
     public int maxElementsPerRow = 3;
     public int maxColumns = 3;
 
@@ -22,8 +24,8 @@ public class GeneralPopup : MonoBehaviour
     private int columnSize = 0;
     void Start()
     {
-        rowSize = ((popupElementSize + widthPerElement) + widthPerElement) * maxElementsPerRow;
-        columnSize = ((popupElementSize + widthPerElement) + widthPerElement) * maxColumns;
+        rowSize = ((popupElementSize + widthPerElement)) * maxElementsPerRow;
+        columnSize = ((popupElementSize + widthPerElement)) * maxColumns;
         ingameUIManager = FindAnyObjectByType<IngameUIManager>();
         popupImages = new List<ImageWithState>();
         PushElement(TestSpriteActive, TestSpriteInactive);
@@ -35,13 +37,34 @@ public class GeneralPopup : MonoBehaviour
         PushElement(TestSpriteActive, TestSpriteInactive);
 
         RectTransform backgroundImageRectTransform = transform.Find("BG").GetComponent<RectTransform>();
+        RectTransform backgroundImageOutlineRectTransform = transform.Find("BGOutline").GetComponent<RectTransform>();
+        TextMeshProUGUI tmpText = backgroundImageRectTransform.transform.Find("Title").GetComponent<TextMeshProUGUI>();
+       
+        Button exitButton = backgroundImageRectTransform.transform.Find("ExitButton").GetComponent<Button>();
+      
+        Button actionButton = backgroundImageRectTransform.transform.Find("ActionButton").GetComponent<Button>();
+        TextMeshProUGUI actionButtonTmpText = actionButton.transform.Find("Title").GetComponent<TextMeshProUGUI>();
 
-        backgroundImageRectTransform.sizeDelta = new Vector2(rowSize, columnSize); 
-        backgroundImageRectTransform.anchoredPosition = Vector2.zero;
+        backgroundImageRectTransform.sizeDelta = new Vector2(rowSize, (columnSize + (tmpText.fontSize * 2)) + (actionButtonTmpText.fontSize + 5));
+        backgroundImageRectTransform.anchoredPosition = new Vector2(0, -(actionButtonTmpText.fontSize + 5));
         backgroundImageRectTransform.pivot = new Vector2(0, 0);
         backgroundImageRectTransform.anchorMin = new Vector2(0, 0); 
-        backgroundImageRectTransform.anchorMax = new Vector2(0, 0); 
+        backgroundImageRectTransform.anchorMax = new Vector2(0, 0);
 
+        backgroundImageOutlineRectTransform.sizeDelta = new Vector2(rowSize + 4, ((columnSize + (tmpText.fontSize * 2)) + 4) + (actionButtonTmpText.fontSize + 5));
+        backgroundImageOutlineRectTransform.anchoredPosition = new Vector2(-3, -2 - (actionButtonTmpText.fontSize + 5));
+        backgroundImageOutlineRectTransform.pivot = new Vector2(0, 0);
+        backgroundImageOutlineRectTransform.anchorMin = new Vector2(0, 0);
+        backgroundImageOutlineRectTransform.anchorMax = new Vector2(0, 0);
+
+        exitButton.onClick.AddListener(OnExitButton);
+    }
+
+    public void AddActionButtonListener(UnityAction callBack)
+    {
+        RectTransform backgroundImageRectTransform = transform.Find("BG").GetComponent<RectTransform>();
+        Button actionButton = backgroundImageRectTransform.transform.Find("ActionButton").GetComponent<Button>();
+        actionButton.onClick.AddListener(callBack);
     }
 
     public void PushElement(Sprite activeSprite,Sprite inactiveSprite)
@@ -59,14 +82,14 @@ public class GeneralPopup : MonoBehaviour
         popupImages.Remove(state);
         SetupPositions();
     }
-    void SetupPositions()
+    private void SetupPositions()
     {
         int currentElementInRow = 0;
         int currentColumn = 0;
 
         int currentElementIndex = 0;
 
-        Vector2 thisObjectRectTransformPosition = Vector2.zero;
+        Vector2 thisObjectRectTransformPosition =new Vector2(widthPerElement / 2.0F, widthPerElement / 2.0F);
 
         foreach (var currentItem in popupImages)
         {
@@ -87,7 +110,10 @@ public class GeneralPopup : MonoBehaviour
             ++currentElementIndex;
         }
     }
-
+    private void OnExitButton()
+    {
+        this.gameObject.SetActive(false);
+    }
     // Update is called once per frame
     void Update()
     {
